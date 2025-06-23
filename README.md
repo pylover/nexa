23c56478b33cd96ebd802a7c87b9fc5a52fcd708
pylover <vahid.mardani@gmail.com>
2025-06-23 14:32:29 +0330

    feature: command line interface

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # nexa
 
+## Prerequisites
+- https://github.com/pylover/yacap
+- https://github.com/pylover/clog
+
 ## Build
 ```bash
 mkdir build && cd build
--- main.c
+++ main.c
@@ -16,17 +16,76 @@
  *
  *  Author: Vahid Mardani <vahid.mardani@gmail.com>
  */
+/* standard */
 #include <stdio.h>
 #include <stdlib.h>
 
+/* platform */
+
+/* third party */
+#include <clog.h>
+#include <yacap.h>
+
+/* local */
 #include "include/nexamine.h"
+#include "config.h"
 
 
-static struct nexamine n;
+static int
+_run(const struct yacap *c, const struct yacap_command *cmd) {
+    INFO("%s v%s", PROJECT_NAME, PROJECT_VERSION);
+    yacap_help_print(c);
+    return 0;
+}
+
+
+static int
+_main(const struct yacap *c, const struct yacap_command *cmd) {
+    yacap_help_print(c);
+    // yacap_commandchain_print(STDERR_FILENO, c);
+    // dprintf(STDERR_FILENO, ": Invalid command\n");
+    // yacap_try_help(c);
+    return 0;
+}
+
+
+/* create and configure yacap structures */
+static struct yacap_command
+_cli_run = {
+    .name = "run",
+    .entrypoint = _run,
+};
+
+
+static struct yacap
+_cli = {
+    .commands = (const struct yacap_command*[]) {
+        &_cli_run,
+        NULL
+    },
+    .entrypoint = _main,
+};
 
 
 int
-main(int argc, char *argv[]) {
-    return nexamine(&n);
-    // return EXIT_FAILURE;
+main(int argc, const char **argv) {
+    int ret = EXIT_FAILURE;
+    const struct yacap_command *cmd;
+    enum yacap_status status = yacap_parse(&_cli, argc, argv, &cmd);
+
+    if (status == YACAP_OK_EXIT) {
+        ret = EXIT_SUCCESS;
+        goto terminate;
+    }
+
+    if ((status == YACAP_OK) && cmd) {
+        if (cmd->entrypoint == NULL) {
+            goto terminate;
+        }
+        ret = cmd->entrypoint(&_cli, cmd);
+    }
+
+terminate:
+    yacap_dispose(&_cli);
+    return ret;
 }
